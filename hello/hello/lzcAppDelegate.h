e150f07aac685cf073b09dd6c7b74e5f98f89ac4
lzc123 <lanou3g@12345.local>
2014-06-10 16:39:11 +0800

    1.0
    第一天

---
+++ hello/hello/lzcAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  lzcAppDelegate.h
+//  hello
+//
+//  Created by lzc on 14-6-10.
+//  Copyright (c) 2014年 lanou3g. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface lzcAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ hello/hello/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  hello
+//
+//  Created by lzc on 14-6-10.
+//  Copyright (c) 2014年 lanou3g. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "lzcAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([lzcAppDelegate class]));
+    }
+}
